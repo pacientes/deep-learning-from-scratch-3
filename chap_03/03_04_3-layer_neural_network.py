07e210a0fec37a9df08998786946bf4aded1566b
pacientes <pacientes.jw@gmail.com>
2020-11-24 12:48:53 +0000

    Repo Update 2020-11-24 12:48:52

--- chap_03/03_04_3-layer_neural_network.py
+++ chap_03/03_04_3-layer_neural_network.py
@@ -70,22 +70,3 @@ network = init_network()
 x = np.array([1.0, 0.5])
 y = forward(network, x)
 print(y)
-
-# %% softmax
-a = np.array([0.3, 2.9, 4.0])
-exp_a = np.exp(a)
-print(exp_a)
-
-sum_exp_a = np.sum(exp_a)
-print(sum_exp_a)
-
-y = exp_a / sum_exp_a
-print(y)
-
-# %% define softmax function
-def softmax(a):
-    exp_a = np.exp(a)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-
-    return y
