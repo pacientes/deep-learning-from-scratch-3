4b5bb10add4b4be4ebc512aefcb5fcf824d9e2b0
pacientes <pacientes.jw@gmail.com>
2020-11-24 05:42:25 +0000

    Repo Update 2020-11-24 05:42:25

--- chap_01/1_05_numpy.py
+++ chap_01/1_05_numpy.py
@@ -43,4 +43,34 @@ print(A * B)
 print(A)
 print(A * 10)
 
+# %% broadcast
+A = np.array([[1, 2], [3, 4]])
+B = np.array([10, 20])
+print(A * B)
+
+# %% indexing
+X = np.array([[51, 55], [14, 19], [0, 4]])
+print(X)
+
+print(X[0])
+
+print(X[0][1])
+
+# %% indexing using for loop
+for row in X:
+    print(row)
+
+# %% indexing other method
+# X Array to one dimension array
+X = X.flatten()
+print(X)
+
+# index 0, 2, 4
+X[np.array([0, 2, 4])]
+
+# %% indexing for boolean condition
+print(X > 15)
+
+print(X[X > 15])
+
 # %%
