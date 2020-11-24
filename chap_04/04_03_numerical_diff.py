b4706ceab868ed3b38df4cbb6b2bc87d1333b309
pacientes <pacientes.jw@gmail.com>
2020-11-24 15:56:01 +0000

    Repo Update 2020-11-24 15:56:00

--- chap_04/04_03_numerical_diff.py
+++ chap_04/04_03_numerical_diff.py
@@ -74,4 +74,10 @@ def numerical_gradient(f, x):
     return grad
 
 
+# %% numerical gradient example
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+
+
 # %%
