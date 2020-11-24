9b7be98fb5a5620348f186b9926638f0470c25cd
pacientes <pacientes.jw@gmail.com>
2020-11-24 07:19:34 +0000

    Repo Update 2020-11-24 07:19:33

---
+++ chap_02/2_03_perceptron.py
@@ -0,0 +1,69 @@
+# %% simple perceptron - AND gate
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+
+# %% test AND gate
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
+
+# %% add weights and bias
+import numpy as np
+x = np.array([0, 1])
+w = np.array([0.5, 0.5])
+b = -0.7
+print(w*x)
+
+print(np.sum(w*x))
+
+print(np.sum(w*x) + b)
+
+# %% simple perceptron - weights and bias apply AND gate
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+# %% simple perceptron - weights and bias apply NAND and OR gate
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+# %% composite AND & NAND& OR gates, so we get XOR gate
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(XOR(0, 0))
+print(XOR(1, 0))
+print(XOR(0, 1))
+print(XOR(1, 1))
---
+++ chap_03/3_02_activation_function.py
@@ -0,0 +1,8 @@
+# %% step function
+def step_function(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
+
+# %%
