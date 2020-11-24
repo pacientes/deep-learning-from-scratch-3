dc1d5364b62c170fba74ef10612831bc2fba97e9
pacientes <pacientes.jw@gmail.com>
2020-11-24 12:56:32 +0000

    Repo Update 2020-11-24 12:56:32

--- chap_03/03_05_design_output_layer.py
+++ chap_03/03_05_design_output_layer.py
@@ -18,3 +18,28 @@ def softmax(a):
     y = exp_a / sum_exp_a
 
     return y
+
+# %% softmax function, inf problem
+a = np.array([1010, 1000, 990])
+print(np.exp(a) / np.sum(np.exp(a)))
+
+c = np.max(a)
+print(a - c)
+
+print(np.exp(a - c) / np.sum(np.exp(a - c)))
+
+# %% modified softmax function
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    
+    return y
+
+# %% softmax function's feature
+a = np.array([0.3, 2.9, 4.0])
+y = softmax(a)
+print(y)
+
+print(np.sum(y))
