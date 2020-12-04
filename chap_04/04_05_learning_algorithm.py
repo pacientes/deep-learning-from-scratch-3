783511a12668a6f406831ed5f9a9eb2d02f8fd88
pacientes <pacientes.jw@gmail.com>
2020-12-04 04:53:23 +0000

    Repo Update 2020-12-04 04:53:23

--- chap_04/04_05_learning_algorithm.py
+++ chap_04/04_05_learning_algorithm.py
@@ -46,7 +46,7 @@ class TwoLayerNet:
         grads = {}
         grads["W1"] = numerical_gradient(loss_W, self.params["W1"])
         grads["b1"] = numerical_gradient(loss_W, self.params["b1"])
-        grads["W2"] = numerical_gradient(loss_W, self.params["Ww"])
+        grads["W2"] = numerical_gradient(loss_W, self.params["W2"])
         grads["b2"] = numerical_gradient(loss_W, self.params["b2"])
 
         return grads
@@ -60,4 +60,53 @@ print(net.params["b1"].shape)
 print(net.params["W2"].shape)
 print(net.params["b2"].shape)
 
+# %% dummy data
+
+x = np.random.rand(100, 784)  # dummy data
+y = net.predict(x)
+
+# %% dummy data to gradient
+
+x = np.random.rand(100, 784)
+t = np.random.rand(100, 10)
+
+grads = net.numerical_gradient(x, t)  # calc gradient
+
+print(grads["W1"].shape)
+print(grads["b1"].shape)
+print(grads["W2"].shape)
+print(grads["b2"].shape)
+
+# %% mini-batch
+import numpy as np
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+# hyper parameter
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iters_num):
+    # get mini-batch
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    # calc gradient
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    # update weights
+    for key in ("W1", "b1", "W2", "b2"):
+        network.params[key] -= learning_rate * grad[key]
+
+    # log learning result
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
 # %%
