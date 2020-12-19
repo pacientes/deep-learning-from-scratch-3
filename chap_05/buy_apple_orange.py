328df9237f16319fc3d660f1e6929bf361ec697f
pacientes <pacientes.jw@gmail.com>
2020-12-19 18:25:02 +0000

    Repo Update 2020-12-19 18:25:01

--- chap_05/buy_apple_orange.py
+++ chap_05/buy_apple_orange.py
@@ -1,1 +1,18 @@
-from .layer_naive import MulLayer, AddLayer
+from layer_naive import MulLayer, AddLayer
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 2
+tax = 1.1
+
+# layers
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
