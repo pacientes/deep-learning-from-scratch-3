70e656c4185a7f81caa27add930cfc37672bd5ce
pacientes <pacientes.jw@gmail.com>
2020-12-19 18:45:27 +0000

    Repo Update 2020-12-19 18:45:27

--- chap_05/layer_naive.py
+++ chap_05/layer_naive.py
@@ -31,3 +31,41 @@ class AddLayer:
         dy = dout * 1
 
         return dx, dy
+
+
+class Relu:
+    def __init__(self) -> None:
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = x <= 0
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
+
+
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self) -> None:
+        super().__init__()
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
