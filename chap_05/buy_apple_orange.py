d32e10bebd4cffe494198e0beb7fbe63aa085125
pacientes <pacientes.jw@gmail.com>
2020-12-19 18:31:55 +0000

    Repo Update 2020-12-19 18:31:54

--- chap_05/buy_apple_orange.py
+++ chap_05/buy_apple_orange.py
@@ -16,3 +16,14 @@ mul_tax_layer = MulLayer()
 apple_price = mul_apple_layer.forward(apple, apple_num)
 orange_price = mul_orange_layer.forward(orange, orange_num)
 all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(f"price: {price}")
+print(f"backward {dapple_num}, {dapple}, {dorange}, {dorange_num}")
