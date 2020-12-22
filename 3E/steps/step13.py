6e1097df7bce4ddca41278f6420d2efec269fc68
pacientes <pacientes.jw@gmail.com>
2020-12-22 10:19:36 +0000

    Repo Update 2020-12-22 10:19:36

---
+++ 3E/steps/step13.py
@@ -0,0 +1,129 @@
+import numpy as np
+import unittest
+
+
+class Variable:
+    def __init__(self, data) -> None:
+        # 데이터 입력 타입 강제화
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)}는 지원하지 않습니다.")
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    # 계산 효율을 위해 재귀 -> 반복문 방식으로 고친다.
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()  # 함수를 가져온다.
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                x.grad = gx
+
+                if x.creator is not None:
+                    funcs.append(x.creator)
+
+
+# 타입 변환 편의함수
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    # 가변 길이 인수 전달
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+
+        outputs = [Variable(as_array(y)) for y in ys]
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs  # 출력 저장
+        # outputs에 원소가 하나라면, 리스트가 아닌 원소 자체를 반환
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError
+
+    def backward(self, gys):
+        raise NotImplementedError
+
+
+class Square(Function):
+    def forward(self, xs):
+        return xs ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+# 클래스 생성 편의함수
+def square(x):
+    return Square()(x)
+
+
+class Exp(Function):
+    def forward(self, xs):
+        return np.exp(xs)
+
+    def backward(self, gys):
+        x = self.input.data
+        gx = np.exp(x) * gys
+        return gx
+
+
+# 클래스 생성 편의함수
+def exp(x):
+    return Exp()(x)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+
+# 클래스 생성 편의함수
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+x = Variable(np.array(2.0))
+y = Variable(np.array(3.0))
+
+z = add(square(x), square(y))
+z.backward()
+
+print(z.data)
+print(x.grad)
+print(y.grad)
+
+# step 13의 문제점
+x = Variable(np.array(3.0))
+y = add(x, x)
+print(f"y {y.data}")
+
+y.backward()
+print(f"x.grad {x.grad}")
---
+++ 3E/steps/step16.py
@@ -0,0 +1,159 @@
+import numpy as np
+import unittest
+
+
+class Variable:
+    def __init__(self, data) -> None:
+        # 데이터 입력 타입 강제화
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)}는 지원하지 않습니다.")
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1  # 세대를 기록한다. (부모 + 1)
+
+    # 계산 효율을 위해 재귀 -> 반복문 방식으로 고친다.
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()  # 함수를 가져온다.
+            gys = [output.grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+    def cleargard(self):
+        self.grad = None
+
+
+# 타입 변환 편의함수
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    # 가변 길이 인수 전달
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs  # 출력 저장
+        # outputs에 원소가 하나라면, 리스트가 아닌 원소 자체를 반환
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError
+
+    def backward(self, gys):
+        raise NotImplementedError
+
+
+class Square(Function):
+    def forward(self, xs):
+        return xs ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+# 클래스 생성 편의함수
+def square(x):
+    return Square()(x)
+
+
+class Exp(Function):
+    def forward(self, xs):
+        return np.exp(xs)
+
+    def backward(self, gys):
+        x = self.input.data
+        gx = np.exp(x) * gys
+        return gx
+
+
+# 클래스 생성 편의함수
+def exp(x):
+    return Exp()(x)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+    def backward(self, gy):
+        return gy, gy
+
+
+# 클래스 생성 편의함수
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+# 코드 테스트 - 더미 제너레이션 준비
+generations = [2, 0, 1, 4, 2]
+funcs = []
+
+for g in generations:
+    f = Function()
+    f.generation = g
+    funcs.append(f)
+
+print([f.generation for f in funcs])
+
+funcs.sort(key=lambda x: x.generation)  # 리스트 정렬
+print([f.generation for f in funcs])
+
+f = funcs.pop()
+print(f.generation)
+
+
+# 코드 테스트 - 실제 계산 그래프
+
+x = Variable(np.array(2.0))
+a = square(x)
+y = add(square(a), square(a))
+y.backward()
+
+print(y.data)
+print(x.grad)
