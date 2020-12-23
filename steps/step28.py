4dc52920b481cd07908b50e42d80354441209449
pacientes <pacientes.jw@gmail.com>
2020-12-23 15:46:44 +0000

    Repo Update 2020-12-23 15:46:44

---
+++ steps/step28.py
@@ -0,0 +1,40 @@
+if "__file__" in globals():
+    import os, sys
+
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+
+import numpy as np
+from dezero import Variable
+
+
+def rosenbrock(x0, x1):
+    y = 100 * (x1 - x0 ** 2) ** 2 + (1 - x0) ** 2
+    return y
+
+
+# 함수 테스트
+# x0 = Variable(np.array(0.0))
+# x1 = Variable(np.array(2.0))
+
+# y = rosenbrock(x0, x1)
+# y.backward()
+
+# print(x0.grad, x1.grad)
+
+# 기울기를 사용한 최적화 문제
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+lr = 0.001  # 학습률
+iters = 50000  # 반복 횟수
+
+for i in range(iters):
+    print(x0, x1)
+
+    y = rosenbrock(x0, x1)
+
+    x0.cleargard()
+    x1.cleargard()
+    y.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
