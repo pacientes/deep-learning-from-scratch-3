ccacc0eaa442b04bb40bf7dc7faf66fadd9234ec
pacientes <pacientes.jw@gmail.com>
2020-12-26 19:01:54 +0000

    Repo Update 2020-12-26 19:01:53

--- dezero/layers.py
+++ dezero/layers.py
@@ -10,7 +10,7 @@ class Layer:
         self._params = set()
 
     def __setattr__(self, name, value) -> None:
-        if isinstance(value, Parameter):
+        if isinstance(value, Parameter, Layer):
             self._params.add(name)
         super().__setattr__(name, value)
 
@@ -27,7 +27,12 @@ class Layer:
 
     def params(self):
         for name in self._params:
-            yield self.__dict__[name]
+            obj = self.__dict__[name]
+
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
 
     def cleargrads(self):
         for param in self.params():
