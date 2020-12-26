49820ac0451bcc141e39ce0bd667a2187990f194
pacientes <pacientes.jw@gmail.com>
2020-12-26 16:40:04 +0000

    Repo Update 2020-12-26 16:40:03

--- dezero/functions.py
+++ dezero/functions.py
@@ -148,3 +148,19 @@ def sum_to(x, shape):
     if x.shape == shape:
         return as_variable(x)
     return SumTo(shape)(x)
+
+
+class MatMul(Function):
+    def forward(self, x, W):
+        y = x.dot(W)
+        return y
+
+    def backward(self, gy):
+        x, W = self.inputs
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW
+
+
+def matmul(x, W):
+    return MatMul()(x, W)
