b67c402d5e7a539cf7d316c373f19ebf5d71d34b
pacientes <pacientes.jw@gmail.com>
2020-12-27 18:37:03 +0000

    Repo Update 2020-12-27 18:37:02

--- dezero/layers.py
+++ dezero/layers.py
@@ -1,9 +1,10 @@
-import numpy as np
+import os
 import weakref
-
+import numpy as np
+import dezero.functions as F
 from dezero import cuda
 from dezero.core import Parameter
-import dezero.functions as F
+from dezero.utils import pair
 
 
 class Layer:
@@ -46,6 +47,36 @@ class Layer:
     def to_gpu(self):
         for param in self.params():
             param.to_gpu()
+
+    def _flatten_params(self, params_dict, parent_key=""):
+        for name in self._params:
+            obj = self.__dict__[name]
+            key = parent_key + "/" + name if parent_key else name
+
+            if isinstance(obj, Layer):
+                obj._flatten_params(params_dict, key)
+            else:
+                params_dict[key] = obj
+
+    def save_weights(self, path):
+        self.to_cpu()
+
+        params_dict = {}
+        self._flatten_params(params_dict)
+        array_dict = {key: param.data for key, param in params_dict.items() if param is not None}
+        try:
+            np.savez_compressed(path, **array_dict)
+        except (Exception, KeyboardInterrupt) as e:
+            if os.path.exists(path):
+                os.remove(path)
+            raise
+
+    def load_weights(self, path):
+        npz = np.load(path)
+        params_dict = {}
+        self._flatten_params(params_dict)
+        for key, param in params_dict.items():
+            param.data = npz[key]
 
 
 class Linear(Layer):
